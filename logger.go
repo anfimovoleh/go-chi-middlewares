d79be77b0cc0060da93de1cf299dc42bbbcb9b7e
Oleh Anfimov <41781065+anfimovoleh@users.noreply.github.com>
2025-07-18 23:10:43 +0200

    feat: add options to Logger middleware (#9)

--- logger.go
+++ logger.go
@@ -2,21 +2,55 @@ package chiwares
 
 import (
 	"fmt"
-	"github.com/rs/zerolog"
 	"math/rand"
 	"net/http"
 	"time"
 
+	"github.com/rs/zerolog"
+
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// TODO: add HTTP tests
+// LoggerOption is a function that configures the logger middleware.
+type LoggerOption func(*loggerOptions)
+
+// loggerOptions holds the configuration for the logger middleware.
+type loggerOptions struct {
+	durationThreshold time.Duration
+	ignorePaths       []string
+}
+
+// WithDurationThreshold sets the duration threshold for slow requests.
+func WithDurationThreshold(threshold time.Duration) LoggerOption {
+	return func(o *loggerOptions) {
+		o.durationThreshold = threshold
+	}
+}
+
+// WithIgnorePaths sets the paths to be ignored by the logger.
+func WithIgnorePaths(paths []string) LoggerOption {
+	return func(o *loggerOptions) {
+		o.ignorePaths = paths
+	}
+}
 
 // Logger is a middleware that logs request and response
 // append logger middleware after RequestID middleware
-func Logger(logger zerolog.Logger, durationThreshold time.Duration) func(http.Handler) http.Handler {
+func Logger(logger zerolog.Logger, opts ...LoggerOption) func(http.Handler) http.Handler {
+	options := &loggerOptions{}
+	for _, o := range opts {
+		o(options)
+	}
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			for _, path := range options.ignorePaths {
+				if r.URL.Path == path {
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
+
 			startTS := time.Now()
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
@@ -38,7 +72,7 @@ func Logger(logger zerolog.Logger, durationThreshold time.Duration) func(http.Ha
 
 				lEntry.Debug().Msg("request finished")
 
-				if duration > durationThreshold {
+				if options.durationThreshold > 0 && duration > options.durationThreshold {
 					lEntry.Warn().Any("http_request", r).Msg("slow request")
 				}
 			}()
